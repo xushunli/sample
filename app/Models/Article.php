cf25ee22e384392697c472104ce1f8f66731a25d
vagrant <vagrant@homestead>
2018-01-31 06:17:27 +0000

    Add new Model article

---
+++ app/Models/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 功能：文章模型
+ * 日期：2018/01/31
+ */
+class Article extends Model
+{
+    protected $table = 'my_articles';
+    
+}
